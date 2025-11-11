0db06decaaf00e4020a503231cd8d7f2c8a0e3b5
Aarush Prakash <aarushprakash3@gmail.com>
2025-11-11 15:37:28 -0500

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyCwvIe_lvFD1-xOKWvIMqvBO_VpyRsEotU",
+    authDomain: "qr-code-pro2.firebaseapp.com",
+    projectId: "qr-code-pro2",
+    storageBucket: "qr-code-pro2.firebasestorage.app",
+    messagingSenderId: "473089781163",
+    appId: "1:473089781163:web:77bb432682a0a52449b6ee",
+    measurementId: "G-7GVVYK1JNH"
+  };
